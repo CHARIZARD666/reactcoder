e1cbd37fe7195a4463ccf5128daed8e888426a94
CHARIZARD666 <abe@hotmail.com.ar>
2022-05-17 19:42:45 -0300

    ItemList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar';
+import Persons from './components/Persons/Persons'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Persons></Persons>
+    
+
     </div>
   );
 }
